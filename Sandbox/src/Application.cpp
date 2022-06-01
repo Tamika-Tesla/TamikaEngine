af3bef288c77051bfa351da197ba295281a96c56
tamika <niktesla367@gmail.com>
2022-06-01 13:30:04 -0400

    Project setup

---
+++ Sandbox/src/Application.cpp
@@ -0,0 +1,10 @@
+namespace TamikaEngine {
+
+	__declspec(dllimport) void Print();
+
+}
+
+void main()
+{
+	TamikaEngine::Print();
+}
---
+++ TamikaEngine/src/Test.cpp
@@ -0,0 +1,12 @@
+#include "Test.h"
+#include <stdio.h>
+
+namespace TamikaEngine
+{
+
+	void Print()
+	{
+		printf("Welcome to TamikaEngine!\n");
+	}
+
+}
