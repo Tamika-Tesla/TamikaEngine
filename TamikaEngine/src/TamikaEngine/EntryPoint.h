8ce27aa6d55a5bf1e71000c35c2fad6e115ced76
tamika <niktesla367@gmail.com>
2022-06-13 16:39:42 -0400

    Restore branch

--- TamikaEngine/src/TamikaEngine/EntryPoint.h
+++ TamikaEngine/src/TamikaEngine/EntryPoint.h
@@ -2,7 +2,7 @@
 
 #ifdef TE_PLATFORM_WINDOWS
 
-extern TamikaEngine::Application* TamikaEngine::CreateApplication();
+TamikaEngine::Application* TamikaEngine::CreateApplication();
 
 int main(int argc, char** argv)
 {
--- TamikaEngine/src/Test.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace TamikaEngine {
-
-	__declspec(dllexport) void Print();
-
-}
