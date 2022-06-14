48882dacf13473a752fd60907fdd664f3bf22912
tamika <niktesla367@gmail.com>
2022-06-14 15:20:17 -0400

    Logging system

--- TamikaEngine/src/TamikaEngine.h
+++ TamikaEngine/src/TamikaEngine.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "TamikaEngine/Application.h"
+#include "TamikaEngine/Log.h"
 
 // --------------EntryPoint ---------------------------------------
 #include "TamikaEngine/EntryPoint.h"
--- TamikaEngine/src/TamikaEngine/EntryPoint.h
+++ TamikaEngine/src/TamikaEngine/EntryPoint.h
@@ -2,10 +2,17 @@
 
 #ifdef TE_PLATFORM_WINDOWS
 
-TamikaEngine::Application* TamikaEngine::CreateApplication();
+extern TamikaEngine::Application* TamikaEngine::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	TamikaEngine::Log::Init();
+	TE_CORE_ERROR("Initialize Error Core Log");
+	TE_INFO("Initialize Info Client Log");
+
+	int a = 5;
+	TE_ERROR("Hello! Var={0}", a);
+
 	auto app = TamikaEngine::CreateApplication();
 	app->Run();
 	delete app;
